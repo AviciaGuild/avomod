b5b3b7cd49484e8ff944252ba0583487e2c145d2
albarv340 <54350887+albarv340@users.noreply.github.com>
2021-07-08 01:52:18 +0200

    made /am age show current world's age when no world was specified

--- src/main/java/tk/avicia/avomod/commands/subcommands/AgeCommand.java
+++ src/main/java/tk/avicia/avomod/commands/subcommands/AgeCommand.java
@@ -6,6 +6,7 @@ import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraft.util.text.TextFormatting;
 import org.omg.CosNaming.NamingContextPackage.NotFound;
+import tk.avicia.avomod.Avomod;
 import tk.avicia.avomod.commands.Command;
 import tk.avicia.avomod.utils.Tuple;
 import tk.avicia.avomod.utils.Utils;
@@ -13,14 +14,15 @@ import tk.avicia.avomod.webapi.WorldUpTime;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.UUID;
 
 public class AgeCommand extends Command {
     @Override
     public void execute(MinecraftServer server, ICommandSender sender, String[] params) throws CommandException {
         String outputMessage = "";
+        WorldUpTime worldUpTime = new WorldUpTime();
         if (params.length >= 1) {
             String world = params[0];
-            WorldUpTime worldUpTime = new WorldUpTime();
             try {
                 Tuple<String, Integer> worldAge = worldUpTime.getAge(world);
                 outputMessage = TextFormatting.GOLD + worldAge.x + " : " + TextFormatting.DARK_AQUA +
@@ -29,7 +31,15 @@ public class AgeCommand extends Command {
                 outputMessage = TextFormatting.DARK_RED + params[0] + TextFormatting.RED + " Could not be found!";
             }
         } else {
-            outputMessage = TextFormatting.RED + "age <world>";
+            String name = Avomod.getMC().getConnection().getPlayerInfo(UUID.fromString("16ff7452-714f-3752-b3cd-c3cb2068f6af")).getDisplayName().getUnformattedText();
+            String currentWorld = name.substring(name.indexOf("[") + 1, name.indexOf("]"));
+            try {
+                Tuple<String, Integer> worldAge = worldUpTime.getAge(currentWorld);
+                outputMessage = TextFormatting.GOLD + worldAge.x + " : " + TextFormatting.DARK_AQUA +
+                        Utils.getReadableTime(worldAge.y);
+            } catch (NotFound notFound) {
+                outputMessage = TextFormatting.DARK_RED + currentWorld + TextFormatting.RED + " Could not be found!";
+            }
         }
         TextComponentString textComponent = new TextComponentString(outputMessage);
         sender.sendMessage(textComponent);
