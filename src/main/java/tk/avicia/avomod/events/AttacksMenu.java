c544909295ca9b255935de7d62bf4c0049458f66
newracket <aniket.9upt@gmail.com>
2021-07-14 17:37:07 -0700

    Fixed clicking attacks menu

--- src/main/java/tk/avicia/avomod/events/AttacksMenu.java
+++ src/main/java/tk/avicia/avomod/events/AttacksMenu.java
@@ -95,7 +95,7 @@ public class AttacksMenu {
 
             int width = Avomod.getMC().fontRenderer.getStringWidth(message);
             int x = screenWidth - width - 5;
-            attackCoordinates.put(attack.y, new ScreenCoordinates(0, y, width, y + 12));
+            attackCoordinates.put(attack.y, new ScreenCoordinates(x - 2, y, x + width + 2, y + 12));
 
             Renderer.drawRect(new Color(100, 100, 100, 100), x - 2, y, width + 2, 12);
             Renderer.drawString(message, x, y + 2, new Color(255, 170, 0));
