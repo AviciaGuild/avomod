b4e32837397c7836ff63fe2705e6c9d8118db46c
albarv340 <54350887+albarv340@users.noreply.github.com>
2021-07-17 21:30:53 +0200

    changed version to 1.1

--- src/main/java/tk/avicia/avomod/Avomod.java
+++ src/main/java/tk/avicia/avomod/Avomod.java
@@ -34,7 +34,7 @@ import java.util.Map;
 public class Avomod {
     public static final String MODID = "avomod";
     public static final String NAME = "AvoMod";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.1";
     public static Map<String, Command> commands = new HashMap<String, Command>() {{
         put("help", new HelpCommand());
         put("chestcount", new ChestCountCommand());
