4c7e524530d685103b6e5aa890c0e6fb65aeb6be
newracket <newracket@users.noreply.github.com>
2022-06-15 21:43:56 -0700

    Upped version to 1.6.1

--- src/main/java/tk/avicia/avomod/Avomod.java
+++ src/main/java/tk/avicia/avomod/Avomod.java
@@ -34,7 +34,7 @@ import java.util.concurrent.TimeUnit;
 public class Avomod {
     public static final String MODID = "avomod";
     public static final String NAME = "avomod";
-    public static final String VERSION = "1.6.0";
+    public static final String VERSION = "1.6.1";
     public static Map<String, Command> commands = new HashMap<String, Command>() {{
         put("help", new HelpCommand());
         put("chestcount", new ChestCountCommand());
